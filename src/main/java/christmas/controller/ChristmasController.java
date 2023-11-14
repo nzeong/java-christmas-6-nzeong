fa4f9dcb4645e380bc05c369f6c47d5d21e4a5e1
nzeong <stac_cato@naver.com>
2023-11-14 17:28:55 +0100

    feat: 고객들의 주문 메뉴와 개수를 출력해주는 기능 추가

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,17 +1,25 @@
 package christmas.controller;
 
+import christmas.model.Menu;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
+import java.util.List;
+
 public class ChristmasController {
+
+    private int date;
+    private List<Menu> orders;
+
     public void run() {
         readDate();
         readMenu();
+        printMenu(orders);
     }
 
     private void readDate() {
         try {
-            InputView.readDate();
+            date = InputView.readDate();
         } catch (IllegalArgumentException e) {
             OutputView.printException(e.getMessage());
             readDate();
@@ -20,12 +28,15 @@ public class ChristmasController {
 
     private void readMenu() {
         try {
-            InputView.readMenu();
+            orders = InputView.readMenu();
         } catch (IllegalArgumentException e) {
             OutputView.printException(e.getMessage());
             readMenu();
         }
     }
 
+    private void printMenu(List<Menu> orders){
+        OutputView.printMenu(orders);
+    }
 
 }
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,14 +1,22 @@
 package christmas.view;
 
+import christmas.model.Menu;
+
+import java.util.List;
+
 public class OutputView {
 
     public static void printException(String message) {
         System.out.println(message);
     }
 
-    public void printMenu() {
+    public static void printMenu(List<Menu> orders) {
+        System.out.println("12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n");
         System.out.println("<주문 메뉴>");
-        // ...
+
+        for(int i = 0; i< orders.size(); i++){
+            System.out.println(orders.get(i).getName() + " " + orders.get(i).getCount() + "개");
+        }
     }
-    // ...
+
 }
